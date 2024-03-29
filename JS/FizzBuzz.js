87d840b61fc955d1073687cd5872c5bdf17836a0
bachu24 <peldenchewang2003@gmail.com>
2024-03-29 17:37:36 +0700

    fizzbuzz

---
+++ JS/FizzBuzz.js
@@ -0,0 +1,22 @@
+// call numbers in sequence
+// if multiple of 3 = Fizz
+// if multiple of 5 = Buzz
+// if both = FizzBuzz
+
+var output=[];
+var num=1
+
+function fizzbuzz(){
+    if(num%3 === 0 && num%5===0) {
+        output.push("FizzBuzz");
+    }else if (num%3===0){
+        output.push("Fizz");
+    }else if (num%5===0){
+        output.push("Buzz")
+    }else{output.push(num);
+    }
+    
+    num+=1;
+
+    console.log(output);
+}
