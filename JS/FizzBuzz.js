34794c7821f824918f3da41bea3575668941a749
bachu24 <peldenchewang2003@gmail.com>
2024-03-29 18:00:08 +0700

    fizzbuzz edit

--- JS/FizzBuzz.js
+++ JS/FizzBuzz.js
@@ -5,18 +5,20 @@
 
 var output=[];
 var num=1
-
 function fizzbuzz(){
-    if(num%3 === 0 && num%5===0) {
-        output.push("FizzBuzz");
-    }else if (num%3===0){
-        output.push("Fizz");
-    }else if (num%5===0){
-        output.push("Buzz")
-    }else{output.push(num);
-    }
+    while(num<=15){
     
-    num+=1;
-
+        if(num%3 === 0 && num%5===0) {
+            output.push("FizzBuzz");
+        }else if (num%3===0){
+            output.push("Fizz");
+        }else if (num%5===0){
+            output.push("Buzz")
+        }else{output.push(num);
+        }
+        
+        num++;
+    }
     console.log(output);
+
 }
