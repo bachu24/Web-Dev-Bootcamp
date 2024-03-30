d71822b9ce3e441bfd33e93810cac85b9b85d58c
bachu24 <peldenchewang2003@gmail.com>
2024-03-30 15:56:35 +0700

    DOM 2

--- DOM Challenge Starting Files/index.js
+++ DOM Challenge Starting Files/index.js
@@ -1,13 +1,29 @@
 //only returns single item
-document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "BCP";
-
+//innerHTML - give all the content inside the tag
+//textContent - give only the text content inside the tag
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.textContent = "BCP";
+document.querySelector("h2").innerHTML = "<em>THANK YOU</em>";
 // will give the result in array except for getElementById
 document.getElementsByTagName("li")[1].style.color = "green";
   
-//only the first one from the list
-document.querySelector("ul .list").style.color = "red";
+//only the first one from the list, will only change the bullet color
+document.querySelector("ul .list").style.color = "orange";
 
 //all
-document.querySelectorAll("ul .list").style.color = "red";
+document.querySelectorAll("ul .list")[2].style.color = "pink";
 
+//changing decoration of the anchor tag
+document.querySelector("li a").style.textDecoration = "none";
 
+//background color of the button
+document.querySelector("button").style.backgroundColor = "yellow";
+
+//addding class list
+document.querySelector("button").classList.add("invisible");
+//removing class list
+// document.querySelector("button").classList.remove("invisible"); 
+//toggling class list: if on then off, if off then on
+document.querySelector("button").classList.toggle("invisible");
+
+//attributes; getAttribute; setAttribute
+document.querySelector("li a").setAttribute("href", "https://www.bing.com");
