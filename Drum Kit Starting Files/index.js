2b1e024ec24b20d6bbf63344434cf893bab10de9
bachu24 <peldenchewang2003@gmail.com>
2024-04-21 18:42:56 +0700

    Event Listener

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,15 @@
+for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick)
+}
+
+function handleClick(){
+    alert("I got clicked!");
+}
+
+//anonymouse function
+/**
+ document.querySelector("button").addEventListener("click", function (){
+    alert("I got clicked!");
+})
+**/
+
