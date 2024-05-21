03555df2c6c94030beef25b0359879c98f55c6be
bachu24 <peldenchewang2003@gmail.com>
2024-05-21 18:16:18 +0700

    event listener

--- jQuery/index.js
+++ jQuery/index.js
@@ -6,6 +6,38 @@ $("h1").addClass("big-title margin-50");
 //console.log($("h1").css("color")); -- to get the color of h1
 
 $("h1").text("Bye");
-$("button").html("<em>Hey</em>");
+$("button").html("<em>Click</em>");
 
 $("a").attr("href","https://www.yahoo.com");//changign the attribute of a tag
+
+//event listener
+$("h1").click(function() {
+    $("h1").css("color","purple");
+  
+});
+
+//js event listener
+for(var i=0;i<5;i++){
+    document.querySelectorAll("button")[i].addEventListener("click",function(){
+        document.querySelector("h1").style.color="red";
+    });
+};
+
+//using jQuery, no need to loop through the buttons
+$("button").click(function(){
+    $("h1").css("color","pink");
+});
+
+$("input").keydown(function(event){
+    console.log(event.key);
+});
+
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+});
+
+//easier way to add event listener
+$("h1").on("mouseover",function(){
+    $("h1").css("color","blue");
+})
+
