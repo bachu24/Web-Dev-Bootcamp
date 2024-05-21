166662542869abfad856bd1967a25cd96625e610
bachu24 <peldenchewang2003@gmail.com>
2024-05-21 17:43:12 +0700

    jQuery css&text

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,4 +1,9 @@
 //$(document).ready(function() {} -  wait for jQuery to finish loading, used this if the script is in the head tag
 
 //$ or jQuery
-$("h1").css("color", "red");
+$("h1").addClass("big-title margin-50");
+//console.log($("h1").css("color","red")); -- setting color to red
+//console.log($("h1").css("color")); -- to get the color of h1
+
+$("h1").text("Bye");
+$("button").html("<em>Hey</em>");
