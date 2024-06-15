9a57eeedbafe29d7fa1fc2d83936f8562dc89fba
bachu24 <peldenchewang2003@gmail.com>
2024-06-15 15:20:09 +0700

    passing data

--- Backend/4.2+Passing+Data/index.js
+++ Backend/4.2+Passing+Data/index.js
@@ -7,10 +7,14 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  
+  res.render("index.ejs");
 });
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const num=req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", {nletters:num});
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
