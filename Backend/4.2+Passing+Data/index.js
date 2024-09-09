335c3d8d8107825a6f2829d9a5334fc37ea1ab89
bachu24 <peldenchewang2003@gmail.com>
2024-09-09 22:36:50 +0700

    nothing

--- Backend/4.2+Passing+Data/index.js
+++ Backend/4.2+Passing+Data/index.js
@@ -16,6 +16,6 @@ app.post("/submit", (req, res) => {
 
 });
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
